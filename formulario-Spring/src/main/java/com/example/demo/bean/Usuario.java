0fc11a9761b836970520a9238feaa6107dab57a1
itamar <itamar@example.com>
2023-10-17 15:07:16 +0200

    subiendo proyecto 0

---
+++ formulario-Spring/src/main/java/com/example/demo/bean/Usuario.java
@@ -0,0 +1,24 @@
+package com.example.demo.bean;
+
+// Lo dejo en la pagina 41 del manual.
+
+public class Usuario {
+	private String nombre;
+	private String password;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
